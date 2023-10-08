e13318cf3002eaae6818f24bd6fc4e48f1249ff0
Christian <frezzebrain42@gmail.com>
2023-10-08 15:58:49 -0600

    Reload Quote Functionality & Responsive Design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,14 @@
-let quotesContainer = document.getElementById('quotes-container');
+let btnReload = document.getElementById('btn-reload');
+
+btnReload.addEventListener('click',()=>{
+    reloadQuote();
+    console.log('entrando')
+});
 
 function getQuote() {
     fetch('https://api.quotable.io/random')
         .then(response => response.json())
         .then(data => {
-
             document.getElementById('quote').innerHTML = '"' + data.content + '"';
 
             document.getElementById('quote-author').innerHTML = data.author;
@@ -12,12 +16,18 @@ function getQuote() {
             for(i=0;i<Object.keys(data.tags).length;i++){
                 document.getElementById('quotes-container').innerHTML += '<h4 class="quote-tag">'+data.tags[i]+'</h4>'
             }
-
-            console.log(Object.keys(data.tags).length)
         })
         .catch(error => {
             console.log(error)
         })
 }
 
+function reloadQuote(){
+    removeTags();
+    getQuote();
+}
+
+function removeTags(){
+    document.getElementById('quotes-container').innerHTML = "";
+}
 getQuote();
